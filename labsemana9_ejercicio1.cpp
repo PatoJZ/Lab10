0239390a20518e9c1193ee1a955610d34bc32e4e
PatoJZ <patojimenez08@gmail.com>
2023-05-21 16:05:04 -0400

    final

--- labsemana9_ejercicio1.cpp
+++ labsemana9_ejercicio1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -100,7 +101,7 @@ void printTreePreOrder(Node *root)
 }
 void printTreePostOrder(Node *root)
 {
-     if (root == NULL)
+    if (root == NULL)
     {
         return;
     }
@@ -110,11 +111,70 @@ void printTreePostOrder(Node *root)
 
     cout << root->data << " ";
 }
+Node *DepthSearch(Node *root, int value)
+{
+    if (root == NULL || root->data == value)
+    {
+        return root;
+    }
+    Node *result = NULL;
 
+    // Buscar en el subárbol izquierdo
+    result = DepthSearch(root->left, value);
 
+    if (result != NULL)
+    {
+        return result;
+    }
+
+    // Buscar en el subárbol derecho
+    result = DepthSearch(root->right, value);
+
+    if (result != NULL)
+    {
+        return result;
+    }
+
+    return NULL;
+}
+
+bool breadthFirstSearch(Node *root, int target)
+{
+    if (root == nullptr)
+    {
+        return false;
+    }
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *current = q.front();
+        q.pop();
+
+        if (current->data == target)
+        {
+            return true;
+        }
+
+        if (current->left != nullptr)
+        {
+            q.push(current->left);
+        }
+
+        if (current->right != nullptr)
+        {
+            q.push(current->right);
+        }
+    }
+
+    return false;
+}
 int main()
 {
-    Node *root; 
+    Node *root = NULL;
+
     root = createNode(1);
     root->left = createNode(2);
     root->right = createNode(3);
@@ -122,19 +182,81 @@ int main()
     root->left->right = createNode(5);
     root->right->left = createNode(6);
     root->right->right = createNode(7);
-    
+    // ejercicio 1 Y 3, la funcion de impresion in order está hecha
+    cout << "- Ejercicio 1 y 3" << endl;
     printTreeInOrder(root);
-    
-    cout << endl << "--------------------------------"<<endl;
-    
+
+    cout << endl
+         << "- Ejercicio 2 --------------------------------" << endl;
+
     printTreePreOrder(root);
 
+    cout << endl
+         << "- Ejercicio 4 --------------------------------" << endl;
 
-    cout << endl << "--------------------------------"<<endl;
-    
-    printTreePostOrder(root);   
+    printTreePostOrder(root);
 
+    cout << endl
+         << "- Ejercicio 5 --------------------------------" << endl;
 
+    // ejercicio 5
+    Node *root1 = createNode(21);
+    int arr[] = {18, 6, 9, 10, 7, 19, 15, 12, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < size; i++)
+    {
+        insertNode(root1, arr[i]);
+    }
+
+    printTreeInOrder(root1);
+
+    // ejercicio 6
+
+    cout << endl
+         << "- Ejercicio 6 --------------------------------" << endl;
+    Node *result = DepthSearch(root1, 9);
+    if (result != NULL)
+    {
+        cout << "Se encontro el valor " << 9 << " en el arbol binario." << endl;
+    }
+    else
+    {
+        cout << "No se encontro el valor " << 9 << " en el arbol binario." << endl;
+    }
+    Node *result2 = DepthSearch(root1, 23);
+    if (result2 != NULL)
+    {
+        cout << "Se encontro el valor " << 23 << " en el arbol binario." << endl;
+    }
+    else
+    {
+        cout << "No se encontro el valor " << 23 << " en el arbol binario." << endl;
+    }
+
+    // ejercicio 7
+
+    cout << "- Ejercicio 7--------------------------------" << endl;
+    bool found = breadthFirstSearch(root, 7);
+
+    if (found)
+    {
+        cout << "El valor " << 7 << " fue encontrado en el arbol." << endl;
+    }
+    else
+    {
+        cout << "El valor " << 7 << " no fue encontrado en el arbol." << endl;
+    }
+
+    bool found2 = breadthFirstSearch(root, 11);
+
+    if (found2)
+    {
+        cout << "El valor " << 11 << " fue encontrado en el arbol." << endl;
+    }
+    else
+    {
+        cout << "El valor " << 11 << " no fue encontrado en el arbol." << endl;
+    }
     return 0;
 }
-
